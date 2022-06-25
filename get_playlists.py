f606adb48cb7b6fc13b7f205a5656d3c50716532
Benedikt Schwab <beneschwab92@gmail.com>
2022-06-25 13:33:18 +0200

    added delte function and setting it to false

--- get_playlists.py
+++ get_playlists.py
@@ -5,8 +5,12 @@ from spotipy.oauth2 import SpotifyOAuth
 from spotipy.oauth2 import SpotifyClientCredentials
 from pathlib import Path
 from config import *
+import shutil
 
 SCOPE = "user-library-read"
+REMOVE = False
+
+EXISTING_PLAYLISTS = os.listdir(SYNC_FOLDER)
 
 # authenticate
 def authenticate():
@@ -32,20 +36,46 @@ def sync_playlist(url, name):
 
 
 # create playlist classes for each playlist
-def download_playlists(playlists, sync_folder= SYNC_FOLDER):
+def sync_playlists(playlists, sync_folder= SYNC_FOLDER):
      for name, url in playlists.items():
           os.chdir(SYNC_FOLDER)
           Path(name).mkdir(parents=True, exist_ok=True)
           os.chdir(os.path.join(SYNC_FOLDER, name ))
           sync_playlist(url, name)
      
-def remove_deleted_playlists():
-     pass
+def fetch_playlists_to_remove(playlists , existing_playlists = set(EXISTING_PLAYLISTS)):
+     playlist_names = set(playlists.keys())
+     playlists_to_remove = existing_playlists.difference(playlist_names)
+    
+     return playlists_to_remove
+
+def remove_deleted_playlists(playlists_to_remove):
+     for name in playlists_to_remove:
+          f = os.path.join(SYNC_FOLDER, name)
+          
+          if  os.path.isdir(f):
+               if input("Remove the playlist: ", name ):
+                    shutil.rmtree(f)
+     return 
 
 def main():
+     # change directory to current working directory
      os.chdir(SYNC_FOLDER)
+     
+     # authenticate user with config file
      sp = authenticate()
+
+     # get all playlists from user
      playlists = get_playlists(sp)
-     download_playlists(playlists)
+
+     # remove delted playlists
+
+     if REMOVE:
+          playlists_to_remove = fetch_playlists_to_remove(playlists)
+          remove_deleted_playlists(playlists_to_remove)
+
+     # download or sync playlists
+     sync_playlists(playlists)
 
 main()
+
